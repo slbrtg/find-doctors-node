1b5d5e0043fdc97f3e8665d5731d757997b6cff7
Saul Artiga <slbrtg@gmail.com>
2017-08-04 17:33:11 -0700

    create functioning display of a few doctors in the pdx area

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,23 @@
 const Doctor = require('./../js/doctor.js').DoctorModule;
 const apiKey = require('./../.env').apiKey;
 
-function displayDefault(firstName, lastName, ){
-
+function displayData(fullName, specialties, offices){
+  let specialtiesString = specialties.join(",");
+  let officesString = offices.join("<br>");
+  $('.doctors').append(
+    "<div class = 'doctor'>" +
+    "<h1>Dr." +
+    fullName +
+    "</h1>" +
+    "<h2>Specialty: " +
+    specialtiesString +
+    "</h2>" +
+    "<h3>Office Information: " +
+    officesString +
+    "</h3><hr></div>");
 }
 
 $(document).ready(function(){
   const doctor = new Doctor();
-  doctor.findDoctor("");
+  doctor.findDoctor("", displayData);
 });
--- js/doctor.js
+++ js/doctor.js
@@ -4,24 +4,34 @@ function Doctor(){
 
 }
 
-Doctor.prototype.findDoctor = function(medicalIssue){
+Doctor.prototype.findDoctor = function(medicalIssue, displayData){
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
       let fullName = "";
       let specialties = [];
       let offices = [];
-      for (let x = 0; x < 20; x++){
-        fullName = result.data[x]["profile"]["first_name"]
-        + " " + result.data[x]["profile"]["last_name"];
+      for (let x = 12; x <= 19; x++){
+        let specialtiesLength = 0;
+        let officesLength = 0;
+        fullName = result.data[x]["profile"]["first_name"] +
+        " " +
+        result.data[x]["profile"]["last_name"];
 
-        for (let y = 0; y < result.data[0]["specialties"].length; y++){
+        for (let y = 0; y <= specialtiesLength; y++){
           specialties.push(result.data[x]["specialties"][y]["name"]);
         }
+        for (let z = 0; z <= officesLength; z++){
+          offices.push(result.data[x]["practices"][z]["name"]);
+          offices.push(result.data[x]["practices"][z]["location_slug"]);
+        }
+        displayData(fullName, specialties, offices);
+        specialties = [];
+        offices = [];
       }
     })
    .fail(function(error){
-      console.log("fail");
+      $('.doctors').append("<h1>There was an error pulling all doctor information</h1>");
     });
 };
 exports.DoctorModule = Doctor;
